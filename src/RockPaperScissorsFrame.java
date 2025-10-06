024ce1b0cd1f6c76cfd89eb4da999b5c20819d90
Zoe Aspenns <aspennza@mail.uc.edu>
2025-10-06 15:10:03 -0400

    JavaDoc complete through RockPaperScissorsFrame

--- src/RockPaperScissorsFrame.java
+++ src/RockPaperScissorsFrame.java
@@ -6,73 +6,157 @@ import java.awt.event.ActionListener;
 import java.util.Random;
 
 
-//Maybe try to make the interface slightly prettier
 //write javadoc
 //create UML diagrams
 
+/**
+ * Creates an extension of the JFrame class called RockPaperScissorsFrame. Users can select either rock, paper, or scissors
+ * and play against a series of algorithms based on the Strategy interface.
+ * @author Zoe Aspenns aspennza@mail.uc.edu
+ */
 public class RockPaperScissorsFrame extends javax.swing.JFrame
 {
+    //A JPanel containing every other GUI element in the JFrame
     JPanel mainPnl;
+
+    //A JPanel containing a JLabel with the title of the game
     JPanel titlePnl;
+
+    //A JPanel containing the Rock, Paper, and Scissors buttons and their ActionListener
     JPanel rpsPnl;
+
+    //A JPanel that tracks and displays the number of wins, ties, losses, and games
     JPanel statsPnl;
+
+    //A JPanel that displays the results of each individual game and what strategy the computer used
     JPanel resultsPnl;
 
+    //A JLabel that displays the name of the game
     JLabel titleLbl;
+
+    //A Font for styling the titleLbl
     Font titlePnlFont;
 
+    //A JButton representing rock as the player's choice
     JButton rockBtn;
+
+    //A JButton representing paper as the player's choice
     JButton paperBtn;
+
+    //A JButton representing scissors as the player's choice
     JButton scissorsBtn;
+
+    //A JButton for quitting the game and closing the JFrame
     JButton quitBtn;
 
+    //An ImageIcon for the rock button
     ImageIcon rockIcon;
+
+    //An ImageIcon for the paper button
     ImageIcon paperIcon;
+
+    //An ImageIcon for the scissors button
     ImageIcon scissorsIcon;
+
+    //An ImageIcon for the quit button
     ImageIcon quitIcon;
 
+    //A String for storing the player's move choice
     String playerMove;
 
+    //An int tracking how many times the player has played Rock
     int playerRockCount;
+
+    //An int tracking how many times the player has played Paper
     int playerPaperCount;
+
+    //An int tracking how many times the player has played Scissors
     int playerScissorsCount;
 
+    //An int tracking how many times the computer has played Rock
     int compRockCount;
+
+    //An int tracking how many times the computer has played Paper
     int compPaperCount;
+
+    //An int tracking how many times the computer has played Scissors
     int compScissorsCount;
 
+    //An int tracking how many times the computer has won
     int compWins;
+
+    //An int tracking how many times the player has won
     int playWins;
+
+    //An int tracking how many games were ties
     int ties;
+
+    //An int tracking how many total games were played
     int totalGamesPlayed;
 
+    //A String tracking the computer's last move
     String lastCompMove = "";
+
+    //A String tracking the player's last move
     String lastPlayerMove = "";
+
+    //A String tracking what strategy the computer used on a particular turn
     String compStrategy = "";
 
+    //A JLabel for the playerWins text field
     JLabel playerWinsLbl;
+
+    //A JLabel for the computerWins text field
     JLabel computerWinsLbl;
+
+    //A JLabel for the ties text field
     JLabel tiesLbl;
+
+    //A JLabel for the totalGamesPlayed text field
     JLabel totalGamesPlayedLbl;
 
+    //A JTextField displaying how many times the player has won
     JTextField playerWinsTF;
+
+    //A JTextField displaying how many times the computer has won
     JTextField computerWinsTF;
+
+    //A JTextField displaying how many times the game has tied
     JTextField tiesTF;
+
+    //A JTextField displaying how many games have been played
     JTextField totalGamesPlayedTF;
 
+    //A JTextArea displaying the result of every game played
     JTextArea resultsTA;
+
+    //A JScrollPane to allow the resultsTA to scroll
     JScrollPane scroller;
 
+    //An instance of the computer's CheatStrategy class
     CheatStrategy cheatStrat = new CheatStrategy();
+
+    //An instance of the computer's RandomStrategy class
     RandomStrategy randomStrat = new RandomStrategy();
 
+    /**
+     * A class implementing the Strategy interface. Determines the computer's move based on
+     * the player's least used move and selects the move that beats it.
+     * @author Zoe Aspenns aspennza@mail.uc.edu
+     */
     class LeastUsed implements Strategy
     {
+        /**
+         * This method calculates the player's least used move and returns the move that beats it.
+         * @param playerMove the player's selected move
+         * @return A String representing Rock, Paper, or Scissors
+         */
         @Override
         public String getMove(String playerMove)
         {
             int min = Math.min(playerRockCount, Math.min(playerPaperCount, playerScissorsCount));
 
+            //This algorithm selects a move for the computer that will beat the player's least used move
             if(playerRockCount == min)
                 return "P";
             else if (playerPaperCount == min)
@@ -81,15 +165,28 @@ public class RockPaperScissorsFrame extends javax.swing.JFrame
                 return "R";
         }
     }
+
+
+    //An instance of the LeastUsed class
     LeastUsed leastUsedStrategy = new LeastUsed();
 
+    /**
+     * A class implementing the Strategy interface. Determines the player's most used move and returns the move that will beat it.
+     * @author Zoe Aspenns aspennza@mail.uc.edu
+     */
     class MostUsed implements Strategy
     {
+        /**
+         * This method determines which move the player has used most and returns the move that beats it.
+         * @param playerMove the player's selected move
+         * @return a String representing rock, paper, or scissors
+         */
         @Override
         public String getMove(String playerMove)
         {
             int max = Math.max(playerRockCount, Math.max(playerPaperCount, playerScissorsCount));
 
+            //An algorithm that returns the computer's move based on the player's most used move
             if(playerRockCount == max)
                 return "P";
             else if (playerPaperCount == max)
@@ -98,18 +195,34 @@ public class RockPaperScissorsFrame extends javax.swing.JFrame
                 return "R";
         }
     }
+
+    //An instance of the MostUsed class
     MostUsed mostUsedStrategy = new MostUsed();
 
+    /**
+     * A class implementing the Strategy interface. Determines the player's last used move and returns the same move.
+     * @author Zoe Aspenns aspennza@mail.uc.edu
+     */
     class LastUsed implements Strategy
     {
+        //A String storing the computer's chosen move
         String compMove = "";
 
+        /**
+         * A method that determines whether the player has moved, and, if they have, sets the computer's move equal to the player's last move.
+         * @param playerMove the player's chosen move
+         * @return A String representing rock, paper, or scissors
+         */
         @Override
         public String getMove(String playerMove)
         {
+            //A random number generator
             Random gen = new Random();
+
+            //An int that will hold a randomly generated index representing rock, paper, or scissors
             int rpsIndex = 0;
 
+            //An algorithm for determining whether the player has moved yet; if yes, the computer's move is set equal to the player's last; if no, the computer's move is randomized
             if(!lastPlayerMove.isEmpty())
             {
                 compMove = lastPlayerMove;
@@ -117,6 +230,7 @@ public class RockPaperScissorsFrame extends javax.swing.JFrame
             {
                 rpsIndex = gen.nextInt(3);
 
+                //This algorithm determines the computer's move based on the random index generated
                 switch (rpsIndex)
                 {
                     case 0:
@@ -134,15 +248,27 @@ public class RockPaperScissorsFrame extends javax.swing.JFrame
             return compMove;
         }
     }
+
+    //An instance of the LastUsed class
     LastUsed lastUsedStrategy = new LastUsed();
 
+    /**
+     * This method randomizes the strategy that the computer will use based on a randomly-generated probability.
+     * @param playerMove the player's selected move
+     * @return The computer's selected move
+     */
     public String getComputerMove(String playerMove)
     {
+        //A random number generator
         Random gen = new Random();
+
+        //An integer representing a random probability
         int prob = gen.nextInt(100) + 1;
 
+        //A String containing the computer's move choice
         String compMove = "";
 
+        //An algorithm that determines the computer's strategy based on the probability generated
         if(prob <= 10)
         {
             compStrategy = "Cheat";
@@ -165,6 +291,7 @@ public class RockPaperScissorsFrame extends javax.swing.JFrame
             compMove = randomStrat.getMove(playerMove);
         }
 
+        //An algorithm that tracks the number of times the computer has used a specific move and the move it used last
         switch (compMove)
         {
             case "R":
@@ -184,15 +311,24 @@ public class RockPaperScissorsFrame extends javax.swing.JFrame
         return compMove;
     }
 
+    /**
+     * This method cross-references the player's move with the computer's move to determine which has won.
+     * @param playerMove the player's chosen move
+     */
     public void resolve(String playerMove)
     {
+        //This String stores the computer's move
         String compMove = getComputerMove(playerMove);
+
+        //This String stores the text that will be output depending on what win condition has occurred
         String result = "";
 
         resultsTA.append("Player: " + playerMove + "; Computer: " + compMove + "\n");
 
+        //This algorithm identifies the player's move
         if(playerMove.equals("R"))
         {
+            //This algorithm identifies the computer's move and determines what win condition has occurred
             if(compMove.equals("R"))
             {
                 result = "Rock vs. Rock! It's a tie!";
@@ -211,6 +347,7 @@ public class RockPaperScissorsFrame extends javax.swing.JFrame
             }
         } else if (playerMove.equals("P"))
         {
+            //This algorithm identifies the computer's move and determines what win condition has occurred
             if(compMove.equals("R"))
             {
                 result = "Paper covers Rock! Player wins!";
@@ -229,6 +366,7 @@ public class RockPaperScissorsFrame extends javax.swing.JFrame
             }
         } else
         {
+            //This algorithm identifies the computer's move and determines what win condition has occurred
             if(compMove.equals("R"))
             {
                 result = "Rock breaks Scissors! Computer wins!";
@@ -252,13 +390,24 @@ public class RockPaperScissorsFrame extends javax.swing.JFrame
         totalGamesPlayedTF.setText(totalGamesPlayed + "");
     }
 
+    /**
+     * This constructor determines the basic settings for the RockPaperScissorsFrame and also calls
+     * all the methods that establish the individual panels in the frame.
+     */
     public RockPaperScissorsFrame()
     {
         super("Rock Paper Scissors Game");
 
+        //This Toolkit is used to find the screen size of the computer running the GUI
         Toolkit kit = Toolkit.getDefaultToolkit();
+
+        //This Dimension stores the screen size
         Dimension screenSize = kit.getScreenSize();
+
+        //This int stores the height of the screen
         int screenHeight = screenSize.height;
+
+        //This int stores the width of the screen
         int screenWidth = screenSize.width;
 
         mainPnl = new JPanel();
@@ -285,6 +434,9 @@ public class RockPaperScissorsFrame extends javax.swing.JFrame
         setVisible(true);
     }
 
+    /**
+     * This method establishes the title panel and the JLabel that it contains
+     */
     private void createTitlePnl()
     {
         titlePnl = new JPanel();
@@ -295,6 +447,9 @@ public class RockPaperScissorsFrame extends javax.swing.JFrame
         titlePnl.add(titleLbl);
     }
 
+    /**
+     * This method establishes the rpsPnl, the buttons and images that it contains, and sets the logic for when each button is clicked.
+     */
     private void createRPSPnl()
     {
         rpsPnl = new JPanel();
@@ -319,15 +474,27 @@ public class RockPaperScissorsFrame extends javax.swing.JFrame
 
         playerMove = "";
 
+        /**
+         * This class creates an ActionListener that determines which button (Rock, Paper, or Scissors)
+         * has been pressed and increments each counter accordingly. Also triggers the resolve method.
+         * @author Zoe Aspenns aspennnza@mail.uc.edu
+         */
         class RPSListener implements ActionListener
         {
+            /**
+             * This method determines which button has been pressed and increments the proper counter.
+             * Also triggers the resolve method.
+             * @param ae the event to be processed
+             */
             @Override
             public void actionPerformed(ActionEvent ae)
             {
                 if (ae.getSource() instanceof JButton selectedButton)
                 {
+                    //This String stores the text of the selected button
                     String buttonText = selectedButton.getText();
 
+                    //This algorithm identifies which button has been pressed and triggers the correct logic
                     switch (buttonText)
                     {
                         case "Rock":
@@ -350,6 +517,7 @@ public class RockPaperScissorsFrame extends javax.swing.JFrame
             }
         }
 
+        //This RPSListener creates an instance of the RPSListener class
         RPSListener listener = new RPSListener();
 
         rockBtn.addActionListener(listener);
@@ -359,8 +527,9 @@ public class RockPaperScissorsFrame extends javax.swing.JFrame
         quitBtn.addActionListener((ActionEvent ae) -> System.exit(0));
     }
 
-
-
+    /**
+     * This method establishes the resultsPnl and the JTextArea and JScrollPane that it contains
+     */
     private void createResultsPnl()
     {
         resultsPnl = new JPanel();
@@ -373,6 +542,9 @@ public class RockPaperScissorsFrame extends javax.swing.JFrame
         resultsPnl.add(scroller);
     }
 
+    /**
+     * This method establishes the statsPnl, as well as the JLabels and JTextFields it contains
+     */
     private void createStatsPnl()
     {
         statsPnl = new JPanel();
@@ -414,7 +586,4 @@ public class RockPaperScissorsFrame extends javax.swing.JFrame
         totalGamesPlayedTF.setText(0 + "");
         statsPnl.add(totalGamesPlayedTF);
     }
-
-
-
 }
--- src/RockPaperScissorsRunner.java
+++ src/RockPaperScissorsRunner.java
@@ -1,9 +1,17 @@
 import javax.swing.*;
 
+/**
+ * Creates an instance of a RockPaperScissorsFrame object so that the RockPaperScissorsFrame.java file can be tested and run.
+ * @author Zoe Aspenns aspennza@mail.uc.edu
+ */
 public class RockPaperScissorsRunner
 {
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args)
     {
+        //This JFrame creates an instance of the RockPaperScissorsFrame class
         JFrame frame = new RockPaperScissorsFrame();
     }
 }
